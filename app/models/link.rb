a5b9d9a765511595b22b9456aa058d509d9ec630
Laurent Bouhnik <misterbl@hotmail.com>
2016-10-18 12:09:24 +0100

    Challenge 09, include Datamapper

--- app/models/link.rb
+++ app/models/link.rb
@@ -4,7 +4,7 @@ require 'dm-postgres-adapter'
 
 class Link
   include DataMapper::Resource
-  
+
   property :id,   Serial
   property :title, String
   property :url, String
@@ -12,6 +12,6 @@ class Link
 end
 
 
-DataMapper.setup(:default, 'postgres://LaurentBo:password@127.0.0.1/bookmark_manager_test')
+DataMapper.setup(:default, 'postgres://postgres/localhost//bookmark_manager_test')
 DataMapper.finalize
 DataMapper.auto_upgrade!
