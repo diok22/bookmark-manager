9b9f24c63fa6cdf1945b6d7a1ccfdb75ee041dab
Dionysis <Dionysis.k22@gmail.com>
2016-10-18 15:03:14 +0100

    database cleaner

--- spec/features/see_links_spec.rb
+++ spec/features/see_links_spec.rb
@@ -3,10 +3,11 @@ require '././app/models/link'
 
 RSpec.feature 'see_links', type: :feature do
   scenario 'shows list of links on the homepage' do
-    visit '/links'
     Link.create(url: 'http://google.com', title: 'Google')
-    # within 'ul#links' do
+    visit '/links'
+    # expect(page.status_code).to be(200)
+    within 'ul#links' do
     expect(page).to have_content('Google')
-    # end
+    end
   end
 end
