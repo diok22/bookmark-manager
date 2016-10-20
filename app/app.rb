540e9ab057162769b3e6ffaef0c7ba971421d6d6
Dionysis <Dionysis.k22@gmail.com>
2016-10-20 12:55:28 +0100

    new user erb form and get method in app

--- app/app.rb
+++ app/app.rb
@@ -24,10 +24,14 @@ class BookmarkManager < Sinatra::Base
     redirect '/links'
   end
 
-  get "/tags/:name" do
+  get '/tags/:name' do
     tag = Tag.first(name: params[:name])
     @links = tag ? tag.links : []
     erb :'links/index'
+  end
+
+  get '/users/new' do
+    erb :'users/new'
   end
 
   # start the server if ruby file executed directly
