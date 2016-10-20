51410e85672a39a49f1356b502040bc601d5ae83
Dionysis <Dionysis.k22@gmail.com>
2016-10-20 12:47:53 +0100

    rspec test for adding a new user

---
+++ spec/features/user_accounts_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+RSpec.feature 'user account management' do
+  scenario 'a new user can sign up' do
+    # user counter will increase by 1
+    expect {sign_up}.to change(User, :count).by(1)
+    expect(page).to have_content('Succesful login, test@tester.com')
+    expect (User.first.email).to eq('test@testercom')
+  end
+end
