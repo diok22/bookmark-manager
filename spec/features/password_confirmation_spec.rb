d71b470f81bd39ebcfec30eb6ff4196950443d37
Dionysis <Dionysis.k22@gmail.com>
2016-10-22 22:38:57 +0100

    rspec test for message to user that passwords do not match

--- spec/features/password_confirmation_spec.rb
+++ spec/features/password_confirmation_spec.rb
@@ -11,4 +11,10 @@ RSpec.feature 'password confirmation' do
 
     expect {click_button 'Create account'}.to change(User, :count).by(0)
   end
+
+  scenario 'with a password that does not match' do
+    expect { sign_up(password_confirmation: 'wrong') }.not_to change(User, :count)
+    expect(current_path).to eq('/users') # current_path is a helper provided by Capybara
+    expect(page).to have_content 'Password and confirmation password do not match'
+  end
 end
